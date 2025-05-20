c05580a18339daed816a05994e16c80b454ed07b
Kauan <kauanlucastoldo@gmail.com>
2025-05-20 11:40:53 -0300

    teste

--- home.js
+++ home.js
@@ -62,6 +62,11 @@ looker.plugins.visualizations.add({
     updateAsync: function(data, element, config, queryResponse, details, done) {
         this.clearErrors();
         this._tableContainer.innerHTML = `
+        <style>
+            #vis {
+                overflow: hidden;
+            }
+        </style>
 
         <div style="overflow-y: hidden;position: relative;z-index: 5; width: 100%; height: 100vh; margin: 0; padding: 0; overflow: hidden; box-sizing: border-box; font-family: 'Montserrat', sans-serif;;
         background-image: url(https://storage.googleapis.com/looker_styles_servopa/imagens/fundo_servopa.png);
