c62c86b9b19f82b883b82274bd9bd931377abf8b
Kauan <kauanlucastoldo@gmail.com>
2025-05-20 11:11:31 -0300

    teste

--- home.js
+++ home.js
@@ -76,7 +76,7 @@ looker.plugins.visualizations.add({
                 <div style="background-color: #ffffff; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); border-radius: 12px; width: 350px; overflow: hidden;">
                     <img src="https://storage.googleapis.com/looker_styles_servopa/imagens/c01-miniatura.jpg" alt="Imagem 1" style="width: 100%; height: 200px; object-fit: cover;">
                     <div style="padding: 20px; text-align: center; display: flex; align-items: center; justify-content: center; gap: 5px;">
-                    <a href="https://servopa.cloud.looker.com/embed/dashboards/80?Bandeira=&Trimestre=&Ano=2025&Periodo=empresa&M%C3%AAs=&Chave%20Empresa=" style="display: flex; align-items: center;font-size: 20px; margin-bottom: 8px; font-weight: bold; color: #333; text-decoration: none; display: block; cursor: pointer;">DRE - Conselho</a>
+                    <a target="_blank" href="https://servopa.cloud.looker.com/embed/dashboards/80?Bandeira=&Trimestre=&Ano=2025&Periodo=empresa&M%C3%AAs=&Chave%20Empresa=" style="display: flex; align-items: center;font-size: 20px; margin-bottom: 8px; font-weight: bold; color: #333; text-decoration: none; display: block; cursor: pointer;">DRE - Conselho</a>
                     <img src="https://cdn-icons-png.flaticon.com/512/5422/5422411.png" alt="Redirecionar" width="25" height="25" style="margin-top: -4px;">
                     </div>
                 </div>
@@ -84,7 +84,7 @@ looker.plugins.visualizations.add({
                 <div style="background-color: #ffffff; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); border-radius: 12px; width: 350px; overflow: hidden;">
                     <img src="https://storage.googleapis.com/looker_styles_servopa/imagens/g01_1-miniatura.jpg" alt="Imagem 2" style="width: 100%; height: 200px; object-fit: cover;">
                     <div style="padding: 20px; text-align: center; display: flex; align-items: center; justify-content: center; gap: 5px;">
-                    <a href="https://servopa.cloud.looker.com/embed/dashboards/92?Bandeira+Bi=&Conta=&Ano=2025&M%C3%AAs=&Chv+Empresa=" style="display: flex; align-items: center; font-size: 20px; margin-bottom: 8px; font-weight: bold; color: #333; text-decoration: none; display: block; cursor: pointer;">DRE - Gerencial</a>
+                    <a target="_blank" href="https://servopa.cloud.looker.com/embed/dashboards/92?Bandeira+Bi=&Conta=&Ano=2025&M%C3%AAs=&Chv+Empresa=" style="display: flex; align-items: center; font-size: 20px; margin-bottom: 8px; font-weight: bold; color: #333; text-decoration: none; display: block; cursor: pointer;">DRE - Gerencial</a>
                     <img src="https://cdn-icons-png.flaticon.com/512/5422/5422411.png" alt="Redirecionar" width="25" height="25" style="margin-top: -6px;">
                     </div>
                 </div>
@@ -92,7 +92,7 @@ looker.plugins.visualizations.add({
                 <div style="background-color: #ffffff; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); border-radius: 12px; width: 350px; overflow: hidden;">
                     <img src="https://storage.googleapis.com/looker_styles_servopa/imagens/g02_1-miniatura.jpg" alt="Imagem 3" style="width: 100%; height: 200px; object-fit: cover;">
                     <div style="padding: 20px; text-align: center; display: flex; align-items: center; justify-content: center; gap: 5px;">
-                    <a href="https://servopa.cloud.looker.com/embed/dashboards/93?Bandeira=&Conta=&Data=1+month" style="display: flex; align-items: center; font-size: 20px; margin-bottom: 8px; font-weight: bold; color: #333; text-decoration: none; display: block; cursor: pointer;">DRE - Objetivo x Realizado</a>
+                    <a target="_blank" href="https://servopa.cloud.looker.com/embed/dashboards/93?Bandeira=&Conta=&Data=1+month" style="display: flex; align-items: center; font-size: 20px; margin-bottom: 8px; font-weight: bold; color: #333; text-decoration: none; display: block; cursor: pointer;">DRE - Objetivo x Realizado</a>
                     <img src="https://cdn-icons-png.flaticon.com/512/5422/5422411.png" alt="Redirecionar" width="25" height="25" style="margin-top: -6px;">
                     </div>
                 </div>
